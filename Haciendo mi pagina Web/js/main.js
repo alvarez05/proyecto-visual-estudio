aeb520430a1c390517bdeba3d6cd2caf7a04c007
alvarez05 <alvarezcyosnai@gmail.com>
2023-10-19 14:41:20 -0500

    cambios

---
+++ Haciendo mi pagina Web/js/main.js
@@ -0,0 +1,5 @@
+//1-implementacion del localStorage(Guardar usuario)
+//2.Redireccionamiento de pagina(Registro- login)
+//3.Nombre en menu de navegacion
+//4.opcion de cerrar sesion en la navegacion 
+//5.validacion del usuario y contraseña en el login 
