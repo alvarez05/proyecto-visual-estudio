82aa1df29f4049da31405192a730bb645549bb2c
alvarez05 <alvarezcyosnai@gmail.com>
2023-10-25 01:39:55 -0500

    guardando progreso en tienda.js

--- Haciendo mi pagina Web/js/tienda.js
+++ Haciendo mi pagina Web/js/tienda.js
@@ -8,14 +8,14 @@ btnCart.addEventListener('click', () => {
 
 
 /* ==================== */
-const  cartInfo = document.querySelector('.cart-product')
-const rowProduc = document.querySelector('.row-product')
+const  cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
 
 // Lista de todos los contenedores de productos
-const productsList = document.querySelector('.container-items')
+const productsList = document.querySelector('.container-items');
 
 // Variable de arreglos de productos
-let allProducts =  [] 
+let allProducts =  []; 
 
 
 
@@ -28,11 +28,38 @@ productsList.addEventListener('click', e => {
         const infoProduct = {
             quantity: 1,
             title: product.querySelector('h2').textContent,
-            price: product.querySelector('p').textContent
-        }
+            price: product.querySelector('p').textContent,
+        };
 
-       allProducts = [...allProducts, infoProduct]
-    }
+       allProducts = [...allProducts, infoProduct];
+    
+        showHTML();
+    } 
+});
 
-    console.log(allProducts)
-})
+// Funcion para mostrar html
+const showHTML = () => {
+
+    // Limpiar HTML 24:47 / 43:48 quede en el video
+    rowProduct.innerHTML = '';
+
+    allProducts.forEach(product => {
+       const containerProduct = document.createElement('div');
+       containerProduct.classList.add('cart-product');
+
+       containerProduct.innerHTML = `
+       
+        <div class="info-cart-product">
+            <span class="cantidad-producto-carrito">${product.quantity}</span>
+            <p class="titulo-producto-carrito">${product.title}</p>
+            <span class="precio-producto-carrito">${product.price}</span>
+        </div> 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+       `; 
+
+       rowProduct.append(containerProduct);
+    });
+    
+}
